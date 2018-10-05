526ce3d2850d23c06b6923e2802a07703cd95520
kukaro <justkukaro@naver.com>
2018-10-05 11:01:36 +0900

    change

--- APIServer/routes/api/urls.js
+++ APIServer/routes/api/urls.js
@@ -10,7 +10,6 @@ router.get('/:username', function (req, res, next) {
     // console.log(username, password);
     rsProject.selectOne(username, (rows) => {
         let data = rows;
-        console.log('hihi');
         console.log(data);
         rsUrls.selectOne(data[0].id, data[0].owner, (rows) => {
             console.log(rows);
