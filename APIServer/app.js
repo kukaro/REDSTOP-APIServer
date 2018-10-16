5fdf534d421d11399ce7a4b894ecb718fe78931d
easyfordev <easyfordev8@gmail.com>
2018-10-16 10:04:24 +0900

    Route 구조 수정

--- APIServer/app.js
+++ APIServer/app.js
@@ -7,15 +7,17 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api/index');
+
 var apiHeaderRouter = require('./routes/api/header');
 var apiListRouter = require('./routes/api/apiList');
 var apiResponseTimeRouter = require('./routes/api/response-time');
-var apiSignIn = require('./routes/api/sigin-in');
-var apiUrls = require('./routes/api/urls');
-var apiScenarios = require('./routes/api/scenarios');
-var apiLatency = require('./routes/api/latency');
-var apiError = require('./routes/api/error');
-var apiAvgTime = require('./routes/api/avgTime');
+// var apiSignIn = require('./routes/api/sigin-in');
+// var apiUrls = require('./routes/api/urls');
+// var apiScenarios = require('./routes/api/scenarios');
+// var apiLatency = require('./routes/api/latency');
+// var apiError = require('./routes/api/error');
+// var apiAvgTime = require('./routes/api/avgTime');
 
 var app = express();
 
@@ -32,15 +34,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', apiRouter);
+
 app.use('/api/header', apiHeaderRouter);
 app.use('/api/apiList', apiListRouter);
 app.use('/api/response-time', apiResponseTimeRouter);
-app.use('/api/v1/sign-in/', apiSignIn);
-app.use('/api/v1/urls/', apiUrls);
-app.use('/api/v1/scenarios', apiScenarios);
-app.use('/api/v1/latency',apiLatency);
-app.use('/api/v1/error', apiError);
-app.use('/api/v1/avgtime', apiAvgTime);
+// app.use('/api/v1/sign-in/', apiSignIn);
+// app.use('/api/v1/urls/', apiUrls);
+// app.use('/api/v1/scenarios', apiScenarios);
+// app.use('/api/v1/latency',apiLatency);
+// app.use('/api/v1/error', apiError);
+// app.use('/api/v1/avgtime', apiAvgTime);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- APIServer/model/rs-project.js
+++ APIServer/model/rs-project.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     post: 3306,
     user: 'root',
-    password: 'root',
+    password: 'easy88ch',
     database: 'mydb'
 });
 
