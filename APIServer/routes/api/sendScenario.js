981e15bc1328874e41395a8eda613a08b591be32
kukaro <justkukaro@naver.com>
2018-10-18 08:11:33 +0900

    compact

--- APIServer/routes/api/sendScenario.js
+++ APIServer/routes/api/sendScenario.js
@@ -5,11 +5,12 @@ var router = express.Router();
 var convert = require('xml-js');
 var queue = require('queue');
 
+var DEBUG = 0;
 
 router.post('/:uid', function (req, res) {
     var jsonData = convert.xml2json(req.body.data, {compact: true});
     console.log(jsonData);
-    jsonData = JSON.parse(jsonData)['xml']['block'];
+    jsonData = JSON.parse(jsonData)['xml'];
     // var jsonKey = Object.keys(jsonData);
     // console.log(jsonData);
     // console.log(jsonKey);
@@ -20,6 +21,15 @@ function makeTree(jsonData){
     console.log(jsonData);
     let returnJson = {};
     let q = queue();
+    q.push(jsonData);
+    while(true){
+        if(DEBUG==1){
+            break;
+        }
+        let tmp = q.pop();
+        console.log(tmp);
+        DEBUG++;
+    }
     console.log(returnJson);
     return returnJson
 }
