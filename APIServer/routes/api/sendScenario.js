e2be29edabdc084b2f0216bc81b169b08485a056
kukaro <justkukaro@naver.com>
2018-10-18 08:04:55 +0900

    compact

--- APIServer/routes/api/sendScenario.js
+++ APIServer/routes/api/sendScenario.js
@@ -3,15 +3,25 @@ var Influx = require('influx');
 const os = require('os');
 var router = express.Router();
 var convert = require('xml-js');
+var queue = require('queue');
 
 
 router.post('/:uid', function (req, res) {
     var jsonData = convert.xml2json(req.body.data, {compact: true});
     console.log(jsonData);
     jsonData = JSON.parse(jsonData)['xml']['block'];
-    var jsonKey = Object.keys(jsonData);
-    console.log(jsonData);
-    console.log(jsonKey);
+    // var jsonKey = Object.keys(jsonData);
+    // console.log(jsonData);
+    // console.log(jsonKey);
+    makeTree(jsonData);
 });
+
+function makeTree(jsonData){
+    console.log(returnJson);
+    let returnJson = {};
+    let q = queue();
+    console.log(returnJson);
+    return returnJson
+}
 
 module.exports = router;
