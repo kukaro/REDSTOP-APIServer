d354afe1effee4425f51d0fac7dcbb663ac75dfd
kukaro <justkukaro@naver.com>
2018-10-17 10:26:51 +0900

    edit log

--- APIServer/app.js
+++ APIServer/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var fs = require('fs');
-var conf = require('./conf');
+var conf = require('./conf/conf');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
