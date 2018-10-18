a282e9be7230e9e699197494f80c62287cfdc30a
easyfordev <easyfordev8@gmail.com>
2018-10-18 09:13:20 +0900

    Add api for tree init data -/api/v1/treeinit

--- APIServer/routes/api/index.js
+++ APIServer/routes/api/index.js
@@ -9,7 +9,7 @@ const avgtime = require('./avgTime');
 const latency = require('./latency');
 const error = require('./error');
 const hooking = require('./hooking');
-
+const treeinit = require('./treeinit');
 
 router.use('/apitest',apitest);
 // router.use('/totalapis',totalapis);
@@ -20,6 +20,7 @@ router.use('/sign-in/', apiSignIn);
 router.use('/urls/', apiUrls);
 router.use('/scenarios', apiScenarios);
 router.use('/hooking',hooking);
+router.use('/treeinit',treeinit);
 
 router.get('/', (req, res) => {
     res.status(200).send({
