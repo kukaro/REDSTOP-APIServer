1b92492862737e9741754ffbd57df1eb5cb42ac8
kukaro <justkukaro@naver.com>
2018-11-08 01:53:25 +0900

    add_api_name_routin

--- APIServer/routes/api/sendScenario.js
+++ APIServer/routes/api/sendScenario.js
@@ -54,9 +54,7 @@ router.post('/:owner/:projectId/:scenarioName', function (req, res, next) {
                     })
                 } else if (atom.type === 'api') {
                     sql = `insert into rs_${projectId}_${scenarioName}_block(type,name,id,parentBlockId,url,method) values(?,?,?,?,?,?)`;
-                    if(atom.name){
-                        atom.name = atom.url;
-                    }
+                    atom.name = atom.url;
                     Database.query(sql, [
                         atom.type,
                         atom.name,
