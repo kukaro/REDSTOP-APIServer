5ba57a0ba17a6662f80731cef292ce6f11b8a9ab
easyfordev <easyfordev8@gmail.com>
2018-11-19 02:53:39 +0900

    Send 5 random numbers

--- APIServer/routes/api/v2/number.js
+++ APIServer/routes/api/v2/number.js
@@ -4,10 +4,16 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var randVal = Math.random()*50 + 1;
     var randVal2 = Math.random()*50 + 1;
+    var randVal3 = Math.random()*50 + 1;
+    var randVal4 = Math.random()*50 + 1;
+    var randVal5 = Math.random()*50 + 1;
 
     res.status(200).send({
         number: Math.round(randVal),
-        number2: Math.round(randVal2)
+        number2: Math.round(randVal2),
+        number3: Math.round(randVal3),
+        number4: Math.round(randVal4),
+        number5: Math.round(randVal5)
     });
 });
 
