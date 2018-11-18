7a62d38d7a1241dc30a843b5e0d64f38cffaa6fc
easyfordev <easyfordev8@gmail.com>
2018-11-19 02:28:35 +0900

    Two random number

--- APIServer/routes/api/v2/number.js
+++ APIServer/routes/api/v2/number.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
     var randVal = Math.random()*50 + 1;
+    var randVal2 = Math.random()*50 + 1;
 
     res.status(200).send({
-        number: Math.round(randVal)
+        number: Math.round(randVal),
+        number2: Math.round(randVal2)
     });
 });
 
