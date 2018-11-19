921ac884cebb84972793e5f9ff3864dc6ac224b0
kukaro <justkukaro@naver.com>
2018-11-20 06:37:44 +0900

    compact

--- APIServer/routes/api/v2/number.js
+++ APIServer/routes/api/v2/number.js
@@ -2,29 +2,70 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-router.get('/', function(req, res) {
+var resultList = [];
+var VUs;
+var responseTime;
+var failures;
+var failuresSlashS;
+var tps;
+var count;
+
+router.get('/', function (req, res) {
     console.log('마이크 테스트');
     let url = 'http://52.79.221.114:3000/api/v1/send-scenario/kukaro/2/default';
     axios.get(url)
-        .then(function(response) {
+        .then(function (response) {
             console.log('response입니다.');
             console.log(Object.keys(response));
             // console.log(response.data);
-            return response.data
+            resultList = [];
+            VUs = 10;
+            responseTime = 0;
+            failures = 0;
+            failuresSlashS = 0;
+            tps = 0;
+            count = 0;
+            return response.data;
         })
         .then(function (data) {
-            console.log('넘어왔어여')
-            console.log(data)
+            console.log('넘어왔어여');
+            let tmp = 0;
+            for (let atom of data) {
+                if (atom.type === 'api') {
+                    tmp++;
+                }
+            }
+            const MAXCOUNT = VUs * tmp;
+            for (let atom of data) {
+                if(atom.type === 'api'){
+                    for (let i = 0; i < VUs; i++) {
+                        axios.post(
+                            'http://52.79.221.114:3000/api/v1/apitest',
+                            {
+                                data: {},
+                                url: atom.url,
+                                method: atom.method
+                            }
+                        ).then(response => {
+                            count++;
+                            console.log('응답 성공 : ' + count);
+                        }).catch(err => {
+                            count++;
+                            console.log('응답 실패 : ' + count);
+                        })
+                    }
+                }
+            }
         })
         .catch(function (error) {
             console.log('error입니다.');
-            console.log(error)
+            console.log(error);
         });
-    var randVal = Math.random()*50 + 1;
-    var randVal2 = Math.random()*50 + 1;
-    var randVal3 = Math.random()*50 + 1;
-    var randVal4 = Math.random()*50 + 1;
-    var randVal5 = Math.random()*50 + 1;
+    var randVal = Math.random() * 50 + 1;
+    var randVal2 = Math.random() * 50 + 1;
+    var randVal3 = Math.random() * 50 + 1;
+    var randVal4 = Math.random() * 50 + 1;
+    var randVal5 = Math.random() * 50 + 1;
 
     res.status(200).send({
         number: Math.round(randVal),
