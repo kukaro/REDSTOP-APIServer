450797c61f4a820436e392c713fc9307a33ab109
kukaro <justkukaro@naver.com>
2018-11-19 18:49:21 +0900

    compact

--- APIServer/routes/api/v2/number.js
+++ APIServer/routes/api/v2/number.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 router.get('/', function(req, res) {
+    console.log('마이크 테스트');
+    let url = 'http://52.79.221.114:3000/api/v1/send-scenario/kukaro/2/default';
+    axios.get(url)
+        .then(function(response) {
+            console.log('response입니다.');
+            console.log(Object.keys(response));
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log('error입니다.');
+            console.log(error)
+        });
     var randVal = Math.random()*50 + 1;
     var randVal2 = Math.random()*50 + 1;
     var randVal3 = Math.random()*50 + 1;
