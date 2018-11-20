81283bfe75c5c2659c186968f35ce009a1678852
easyfordev <easyfordev8@gmail.com>
2018-11-20 18:55:25 +0900

    Create file

--- APIServer/routes/api/v2/index.js
+++ APIServer/routes/api/v2/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const scenario = require('./scenario');
 const number = require('./number');
+const createFile = require('./create-file');
 
 router.use('/scenario',scenario);
 router.use('/number',number);
+router.use('/file',createFile);
 
 
 module.exports = router;
